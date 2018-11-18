a9a9859ca7970a6ea91a8bb2b56a027de2b13ff8
Diego <diego.luz.braga@gmail.com>
2018-11-17 22:26:29 -0300

    - Change: ended all TODOs. - Change: implemented all calculation functions. - Change: implemented dot logic. - Add: alert .xml and .kt. - Change: added alert logic.

--- app/src/main/java/com/example/t_gamer/easycalc/MainActivity.kt
+++ app/src/main/java/com/example/t_gamer/easycalc/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.example.t_gamer.easycalc
 
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.app.AlertDialog
 import android.os.Bundle
-import android.text.Editable
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_alert.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    // TODO - FAZER CALCULOS
 
     private var operator: String = ""
-    private var firstNumber: Double = 0.0
-    private var secondNumber: Double = 0.0
+    private var numberOfDots: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         lessBTN.setOnClickListener { appendOperator((it as Button).text.toString()) }
         multiplicationBTN.setOnClickListener { appendOperator((it as Button).text.toString()) }
         divisionBTN.setOnClickListener { appendOperator((it as Button).text.toString()) }
-        dotBTN.setOnClickListener { appendDot() }
+        dotBTN.setOnClickListener { appendDot((it as Button).text.toString()) }
         clearBTN.setOnClickListener {
             clearFunctions(false)
         }
@@ -55,11 +54,15 @@ class MainActivity : AppCompatActivity() {
             operationEDT.setText("")
             resultTXT.text = ""
             operator = ""
+            numberOfDots = 0
         } else {
             if (operationEDT.text.toString().isNotEmpty()) {
                 val index = operationEDT.text.toString().length
-                if (operationEDT.text.toString().takeLast(index-1).equals(operator)) {
+                if (operationEDT.text.toString().takeLast(index - 1).equals(operator)) {
                     operator = ""
+                }
+                if (operationEDT.text.toString().takeLast(index - 1).equals(".")) {
+                    numberOfDots -= 1
                 }
                 operationEDT.setText(operationEDT.text.toString().dropLast(1))
             }
@@ -67,11 +70,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun appendDot() {
-
+    private fun appendDot(dot: String) {
+        if (operationEDT.text.isNotEmpty()) {
+            val numbers = operationEDT.text.toString().split(operator)
+            if (operator.isEmpty() && ("." in numbers[0]).not() && numberOfDots.equals(0)) {
+                operationEDT.text.append(dot)
+                numberOfDots += 1
+            } else {
+                if (numbers[1].isNotEmpty() && ("." in numbers[1]).not() && numberOfDots < 1) {
+                    operationEDT.text.append(dot)
+                    numberOfDots += 1
+                }
+            }
+        }
     }
 
     private fun appendNumbers(value: String) {
-        operationEDT.append(resultTXT.text)
         operationEDT.append(value)
     }
 
@@ -79,6 +93,18 @@ class MainActivity : AppCompatActivity() {
         if (operationEDT.text.isNotEmpty()) {
             if (operator.isNotBlank() && operationEDT.text.toString().contains(operator)) {
                 operationEDT.setText(operationEDT.text.toString().replace(operator, value))
+                val alert = AlertDialog.Builder(this)
+                with(alert){
+                    setNegativeButton(getString(R.string.alert_negative_button)) { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                }
+                val dialog = alert.create()
+                dialog.setTitle(R.string.alert_title)
+                dialog.run {
+                    setView(layoutInflater.inflate(R.layout.activity_alert, null))
+                    show()
+                }
             } else {
                 operationEDT.append(value)
             }
@@ -87,29 +113,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculate() {
-        if (operator.isNotBlank()) {
-            when (operator) {
-                "+" -> sumFun()
-                "-" -> lessFun()
-                "*" -> multiplicationFun()
-                "/" -> divideFun()
-            }
+        resultTXT.text = ""
+        val numbers = operationEDT.text.toString().split(operator)
+        if (numbers.isNotEmpty()) {
+            if (numbers[0].isNotBlank() && numbers[1].isNotBlank())
+                if (operator.isNotBlank()) {
+                    when (operator) {
+                        "+" -> sumFun(numbers[0].toDouble(), numbers[1].toDouble())
+                        "-" -> lessFun(numbers[0].toDouble(), numbers[1].toDouble())
+                        "*" -> multiplicationFun(numbers[0].toDouble(), numbers[1].toDouble())
+                        "/" -> divideFun(numbers[0].toDouble(), numbers[1].toDouble())
+                    }
+                }
         }
     }
 
-    private fun sumFun() {
-
+    private fun sumFun(firstNumber: Double, secondNumber: Double) {
+        resultTXT.text = firstNumber.plus(secondNumber).toString()
     }
 
-    private fun lessFun() {
-
+    private fun lessFun(firstNumber: Double, secondNumber: Double) {
+        resultTXT.text = firstNumber.minus(secondNumber).toString()
     }
 
-    private fun multiplicationFun() {
-
+    private fun multiplicationFun(firstNumber: Double, secondNumber: Double) {
+        resultTXT.text = firstNumber.times(secondNumber).toString()
     }
 
-    private fun divideFun() {
-
+    private fun divideFun(firstNumber: Double, secondNumber: Double) {
+        resultTXT.text = firstNumber.div(secondNumber).toString()
     }
 }
